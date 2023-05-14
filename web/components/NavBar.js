df3cf6b614d541a02bf796a74dd89dc1774fb21e
colonelxy <harold2@protonmail.com>
2023-05-14 10:53:56 +0300

    built home, navbar

---
+++ web/components/NavBar.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { useAuth } from "../contexts/AuthContext";
+import "../flow/config";
+import styles from "../styles/NavBar.module.css";
+
+
+export default function NavBar() {
+    const { currentUser, logOut, logIn } = useAuth();
+
+    return (
+        <div className={styles.navbar}>
+            <Link href="/">Home</Link>
+            <Link href="/purchase">Purchase</Link>
+            <Link href="/manage">Manage</Link>
+            <button onClick={currentUser.addr ? logOut : logIn}>
+                {currentUser.addr ? "Log Out" : "Log In"}
+            </button>
+        </div>
+    );
+}
---
+++ web/pages/index.js
@@ -0,0 +1,62 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import NavBar from "@/components/NavBar";
+import { getAllDomainInfos } from "@/flow/scripts";
+import styles from "../styles/Home.module.css"
+
+export default function Home() {
+    const [domainInfos, setDomainInfos] = useState([]);
+
+    useEffect(() => {
+        async function fetchDomains() {
+            const domains = await getAllDomainInfos();
+            setDomainInfos(domains)
+        }
+        fetchDomains()
+    }, []);
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Flow Name Service</title>
+                <meta name="description" content="Flow Name Service" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <NavBar />
+
+            <main className={styles.main}>
+                <h1>All Registered Domains</h1>
+
+                <div className={styles.domainsContainer}>
+                    {
+                        domainInfos.length === 0 ? (
+                            <p> No FNS Domains have been registered yet</p>
+                        ) : (
+                            domainInfos.map((di, idx) => (
+                                <div className={styles.domainInfo} key={idx}>
+                                    <p>
+                                        {di:id} - {di.name}
+                                    </p>
+                                    <p>Owner: {di.owner}</p>
+                                    <p>Linked Address: {di.address ? di.address : "None"}</p>
+                                    <!-- Parse the timestamps as human-readable dates -->
+                                    <p>
+                                        Created At: {" "}
+                                        {new Date(parseInt(di.createdAt) * 1000).toLocaleDateString()}
+                                    </p>
+                                    <p>
+                                        Expires At: {" "}
+                                        {new Date(parseInt(di.expiresAt) * 1000).toLocaleDateString()}
+                                    </p>
+
+
+                                </div>
+                            ))
+                        )
+                    }
+                </div>
+            </main>
+        </div>
+    );
+}
